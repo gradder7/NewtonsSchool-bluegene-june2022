836d5eb9ce4b9c49a3e77f9e7440abd7e1a216b1
Himanshu Mehra <mehra3228@gmail.com>
2022-07-24 21:42:21 +0530

    test comit

--- Assignments/ContestQ_24_07_2022.java/EncodeDecode.java
+++ Assignments/ContestQ_24_07_2022.java/EncodeDecode.java
@@ -48,6 +48,7 @@ import java.util.*; // contains Collections framework
 // don't change the name of this class
 // you can add inner classes if needed
 class Main {
+    
     public static void main (String[] args) {
                       // Your code here
         Scanner sc=new Scanner(System.in);
